ffacf7529897c82d466f89d0e825d125ef6bc4ed
chedly-abdennadher <abdennadher.chedly@gmail.com>
2022-12-06 17:24:02 +0100

    5emecommit

--- app/src/main/java/model/Adherent.java
+++ app/src/main/java/model/Adherent.java
@@ -4,7 +4,7 @@ package model;
 
 public class Adherent {
 
-
+private int id_adherent;
 private String nom;
 private String prenom;
 
@@ -19,6 +19,12 @@ public String getPrenom() {
 }
 public void setPrenom(String prenom) {
 	this.prenom = prenom;
+}
+public int getId_adherent() {
+	return id_adherent;
+}
+public void setId_adherent(int id_adherent) {
+	this.id_adherent = id_adherent;
 }
 
 }
